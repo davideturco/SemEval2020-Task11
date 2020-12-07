300efc7a4f4fb97f35d0567b122579cac8ceb60d
davideturco <davide.turco@bristol.ac.uk>
2020-12-07 22:09:57 +0000

    Added function for merging and grouping spans

--- SI/data_processing.py
+++ SI/data_processing.py
@@ -23,4 +23,47 @@ def load_articles(article_folder, techniques_folder):
 
     return articles_content, articles_id, propaganda_techniques_names
 
-def load_spans(spans_folder):
+
+def load_spans(file):
+    """
+    Loads the predicted spans
+    """
+    article_id, span_interval = ([], [])
+    with open(file, 'r', encoding='utf-8') as f:
+        for line in f.readlines():
+            art_id, span_begin, span_end = [int(x) for x in line.rstrip().split('\t')]
+            span_interval.append((span_begin, span_end))
+            article_id.append(art_id)
+
+    return article_id, span_interval
+
+
+def merge_spans(span_intervals):
+    """
+    If an article has overlapping spans, they are merged
+    """
+    temp = sorted(span_intervals)
+    merge = [temp[0]]
+    for el in temp:
+        previous = list(merge[-1])
+        if previous[1] >= el[0]:
+            previous[1] = max(previous[1], el[1])
+            merge.pop()
+            merge.append(previous)
+        else:
+            merge.append(el)
+    return merge
+
+
+def group_spans(article_id, span_intervals):
+    """
+    Groups together spans from the same article
+    """
+    dictionary = dict.fromkeys(article_id, span_intervals)
+    spans = {}
+    for key, value in dictionary.items():
+        spans.setdefault(key, [])
+        spans[key].append(value)
+    return spans
+
+
